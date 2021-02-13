4e44beeb8b31b7c5e3712714381710c33bdaac4a
Inmer Suazo <inmer.suazo@unah.hn>
2021-02-12 22:27:20 -0600

    iniciando la tarea

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void tarea1();
+
+int main() {
+	tarea1();
+	return 0;
+}
---
+++ tarea1.cpp
@@ -0,0 +1,24 @@
+/*Realice un programa en C++ que defina un arreglo de enteros de 12 elementos,
+permita ingresar los valores por teclado, encuentre el valor máximo y lo muestre en
+pantalla.*/
+#include <iostream>
+
+using namespace std;
+
+int tarea1() {
+	int arreglo[12];
+	int mayor = 0;
+	cout<<"A continuacion se le pedira ingresar 12 numeros enteros"<<"\n";
+	for (int x=0; x<12; x++){
+		cout<<"Ingrese el elemento No. "<<x+1<<": ";
+		cin>>arreglo[x];		
+	}
+	for (int y=0; y<12; y++){
+		if (mayor < arreglo [y]){
+		mayor = arreglo [y];
+		}
+	}
+	cout<<"El numero mayor ingresado es: "<<mayor<<"\n";
+	system("pause");
+	return 0;
+}
